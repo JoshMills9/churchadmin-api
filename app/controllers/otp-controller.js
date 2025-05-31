867da1028ed10f35bc03525d3cf41c18f76b405e
JoshMills9 <joshuamills105@gmail.com>
2025-05-31 00:15:57 +0000

    fixde the otp

--- app/controllers/otp-controller.js
+++ app/controllers/otp-controller.js
@@ -1,8 +1,8 @@
 const parsePhoneNumberFromString = require('libphonenumber-js')
+const User = require('../models/usersSchema')
 
 
-
-const generateOtp = (req, res, next) => {
+const generateOtp = async(req, res, next) => {
     const {phoneNumber, churchName} = req.body
 
     if (!phoneNumber) {
@@ -19,14 +19,22 @@ const generateOtp = (req, res, next) => {
      const code = Math.floor(10000 + Math.random() * 90000);
      const number = (formatPhone(phoneNumber))
 
-     const value = {
-        code,
-        number,
-        churchName
-      }
-      res.status(200).json(value)
+    try{
+        const existing = await User.findOne({ phone: phoneNumber });
+        if(!existing){
+            const value = {
+                code,
+                number,
+                churchName
+              }
+              res.status(200).json(value)
+        }else{
+            res.json({error: 'You aleady have an account. Please login'})
+        }
+    }catch(err){
+        res.json(err)
     }
-
+    }
 } 
 
 
--- app/controllers/users-controller.js
+++ app/controllers/users-controller.js
@@ -11,25 +11,20 @@ const getUsers = async (req, res, next) => {
 
 
 const createUser = async (req, res, next) => {
-    const { phoneNumber, churchName } = req.body;
-      
-    try{
-        // Optional: store only unique users or update if they exist
-        const existing = await User.findOne({ phone: phoneNumber });
-        if (!existing) {
-          const createdUser = new User({
-             church: churchName, 
-             phone: phoneNumber,
-             posts: [],
-             events: [],
-             followers: [], 
-            });
-          await createdUser.save();
-          res.json({user: createdUser})
-        }else{
-            res.json({error: 'You aleady have an account. Please login'})
-        }
+  const { phoneNumber, churchName } = req.body;
 
+    try{
+      const createdUser = new User({
+          church: churchName, 
+          phone: phoneNumber,
+          posts: [],
+          events: [],
+          followers: [], 
+            });
+            
+      await createdUser.save();
+      res.json({user: createdUser})
+      
     }catch(err){
         console.log(err)
     }
